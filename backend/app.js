fc13e8dc4720c703b6465c870b2eb0f12972c6cf
bharat882 <bharatsinghsaini00@gmail.com>
2024-05-28 17:04:58 -0400

    feature1-signup API added
    feature1-signup API added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const User = require("./models/userModel");
-
 var express = require("express");
 var app = express();
 var cors = require("cors");
@@ -11,6 +10,10 @@ var login = multer();
 app.use(login.array());
 app.use(bodyParser.json());
 const port = 3000;
+
+// encrypting the password using bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 mongoose
   .connect("mongodb://localhost:27017/e-commerce", {
@@ -24,7 +27,7 @@ mongoose
     console.error("Error connecting to MongoDB", err);
   });
 
-app.post("/signup", function (req, res) {
+app.post("/signup", async function (req, res) {
   console.log("Signup called");
 
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -33,33 +36,34 @@ app.post("/signup", function (req, res) {
   res.setHeader("Access-Control-Allow-Credentials", true);
 
   var username = req.body.username;
-  var passwordHash = req.body.password;
+  var password = req.body.password;
   var email = req.body.email;
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
 
-  const createUser = async () => {
-    try {
-      const newUser = new User({
-        username: username,
-        email: email,
-        passwordHash: passwordHash,
-        firstName: firstName,
-        lastName: lastName,
-        phone: phone,
-      });
+  try {
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-      await newUser.save();
-      console.log("User created successfully");
-    } catch (err) {
-      console.error("Error creating user: ", err);
-    }
-  };
-  createUser();
+    const newUser = new User({
+      username: username,
+      email: email,
+      passwordHash: passwordHash,
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+    });
+
+    await newUser.save();
+    console.log("User created successfully");
+    res.status(200).send("User created successfully");
+  } catch (err) {
+    res.status(400).send("Failed to create user");
+    console.error("Error creating user: ", err);
+  }
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", async function (req, res) {
   console.log("Login called");
 
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -67,9 +71,33 @@ app.post("/login", function (req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  var username = req.body.username;
-  var passwordHash = req.body.password;
-  var email = req.body.email;
+  const { username, password } = req.body;
+
+  console.log("Validating user");
+  try {
+    const user = await User.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      return res.status(400).send("User not found");
+    } else {
+      console.log(user);
+
+      const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+      if (isPasswordValid) {
+        res.status(200).send("Login successful");
+        console.log("User authenticated successfully");
+      } else {
+        console.error("Invalid password");
+        res.status(400).send("Invalid password");
+      }
+    }
+  } catch (err) {
+    console.error("Error during login: ", err);
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.listen(port, () => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,12 +4,13 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
   firstName: { type: String, required: true, unique: true },
   lastName: { type: String, required: true, unique: true },
   phone: { type: Number, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  userType: { type: String, default: "user" },
 });
 
 const User = mongoose.model("User", userSchema);
